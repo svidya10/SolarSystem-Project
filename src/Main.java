663f6e68f9735665b02315731a3f3dc9066d3c5a
svidya10 <svidya10@gmail.com>
2022-12-21 10:17:02 -0800

    Basic solar system structure ready

---
+++ src/Main.java
@@ -0,0 +1,5 @@
+public class Main {
+    public static void main(String[] args) {
+            System.out.println("The basic Structure is ready please write down your code in this Solar project");
+    }
+}
